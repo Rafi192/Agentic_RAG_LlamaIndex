8b188bd0deddb9a1ba0d75eb40d8f385264573ac
Rafi192 <rafihasanmmsc@gmail.com>
2025-11-19 13:00:25 +0600

    Commit of openAI embedder

--- router_query_engine.py
+++ router_query_engine.py
@@ -26,7 +26,7 @@ from llama_index.core import Settings
 splitter = SentenceSplitter(chunk_size=512, chunk_overlap=20)
 
 nodes = splitter.get_nodes_from_documents(documents)
-# print(f"Split documents into {len(nodes)} nodes.", flush=True)
+print(f"Split documents into {len(nodes)} nodes.", flush=True)
 
 #LLM
 Settings.llm = LlamaOpenAI(
@@ -36,10 +36,22 @@ Settings.llm = LlamaOpenAI(
     openai_api_key=os.environ["HF_TOKEN_READ"],
 )
 print("Configured LLM with Qwen3-4B-Instruct-2507:nscale model.", flush=True)
-print(f"LLM Settings: {Settings.llm}", flush=True)
+# print(f"LLM Settings: {Settings.llm}", flush=True)
 #Embedding
+Settings.embed_model = OpenAIEmbedding(model="text-embedding-ada-002")
+print("Configured Embedding model with text-embedding-ada-002.", flush=True)
+# print(f"Embedding Model Settings: {Settings.embed_model}", flush=True)
 
+#defingin summary index and vector index over the same data
+from llama_index.core import SummaryIndex, VectorIndex
+summary_index = SummaryIndex(nodes)
+print("Summary Index created.", flush=True)
+print("Summary index length:", len(summary_index.nodes), flush=True)
+vector_index = VectorIndex(nodes)
+print("Vector Index created.", flush=True)
+print("Vector index length:", len(vector_index.nodes), flush=True)
 
+# 
 
 # these codes are out of date after llama index update
 # service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
