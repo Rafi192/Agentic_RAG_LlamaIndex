ae39593074f8f1ca23e52eaea20e54f39ca77cfe
rafi192 <rafihasanmmsc@gmail.com>
2026-01-08 13:54:52 +0600

    Commit of bangla test

--- router_query/router_query_engine_.py
+++ router_query/router_query_engine_.py
@@ -1,16 +1,32 @@
-from llama_index.core import VectorStoreIndex, SummaryIndex
+from llama_index.core import VectorStoreIndex, SummaryIndex, SimpleDirectoryReader
 from llama_index.core.tools import QueryEngineTool
 from llama_index.core.query_engine import RouterQueryEngine
 from llama_index.core.selectors import LLMSingleSelector
 from llama_index.llms.openai import OpenAI
 from llama_index.llms.google_genai import GoogleGenAI
 from dotenv import load_dotenv
-
+import logging
 load_dotenv()
+logger = logging.getLogger(__name__)
+
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 
 # checking
-print("checking LLM for Gemini")
-llm = GoogleGenAI(model="gemini-2.5-flash")
-response = llm.complete("hello gemini")
-print(response)
-print("gemini response done !")
+print("হ্যালো, তুমি কেমন আছো?")
+# llm = GoogleGenAI(model="gemini-2.5-flash")
+# response = llm.complete("hello gemini")
+# print(response)
+print("প্রতিক্রিয়া সম্পূর্ণ")
+
+#loading docs
+doc_2024 = SimpleDirectoryReader(r"data").load_data()
+if doc_2024:
+    logger.info("directory is read")
+
+else:
+    logger.error("could not load documents")
+
+print("বাংলা ঠিকমতো দেখা যাচ্ছে?")
+print("আমি বাংলায় কথা বলি")
+print("এইটা টেস্ট")
